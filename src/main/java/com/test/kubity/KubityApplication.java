462623e4987aa5f50efec64a6a930421475e2f75
Julien Sergent <sergent.julien.1@gmail.com>
2018-02-09 08:24:40 +0100

    init project

---
+++ src/main/java/com/test/kubity/KubityApplication.java
@@ -0,0 +1,12 @@
+package com.test.kubity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubityApplication.class, args);
+	}
+}
---
+++ src/test/java/com/test/kubity/KubityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.kubity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KubityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
